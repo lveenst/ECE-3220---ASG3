04f0dd907d842959704a78b6ac64e52cc201cf89
asp64 <asp6@g.clemson.edu>
2026-04-09 13:41:47 -0400

    Refactor createRaid5 function and improve input handling

--- Raid5.c
+++ Raid5.c
@@ -9,25 +9,83 @@
  * bugs: N/A
  */
 
+
+
+// could hardcode [0,0],[1,1],[2,2]
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void createRaid5 (char *, int, int){
+void createRaid5 (char *data, int num_stripes, int disk_num){
+    char diskarray[disk_num][num_stripes][4][17];   
+    int total_blocks = 0;
+    int disk = 0;
+    int stripe = 0;
+    int block = 0;
+    int count = 0;
+    char c;
+    int i = 0;
+    
+    memset(diskarray, 0, sizeof(diskarray)); 
+    while (total_blocks < 48) {
+        if (disk == stripe){
+            block = 0;
+            disk++;
+        }else {
+            while(count < 16){
+                c = data[i];
+                if (c == 10){
+                    c = ' ';
+                } else if (c == 32 && count == 0){
+                    c = data[i++];
+                }
+                diskarray[disk][stripe][block][count] = c;
+                count++;
+                i++;
+                
+            }
+            diskarray[disk][stripe][block][16] = '\0';
+            block++;
+            if (block == 4){
+                block = 0;
+                disk++;
+            }
+            if (disk == disk_num){
+                disk = 0;
+                stripe++;
+            }
+            total_blocks++;
+            if (count == 16){
+                count = 0;
+            }
+        }
+        
+    }
+
+
+    for (int j = 0; j < 3; j++) {        
+        for (int i = 0; i < 5; i++) {   
+            for (int k = 0; k < 4; k++) { 
+                printf("disk %d, strip %d, block %d: %s\n", i, j, k, diskarray[i][j][k]);
+            }
+        }
+    }
     return;
 }
 
 void calculateParity ( char * ){
-    char result[17];
-    char a[17];
-    char b[17];
-    char c[17];
-    char d[17];
-    for (int i = 0; i < 16; i++) {
-        result[i] = a[i] ^ b[i] ^ c[i] ^ d[i];
-    }
-    result[16] = '\0';
-    return;
+    // char result[17];
+    // char a[17];
+    // char b[17];
+    // char c[17];
+    // char d[17];
+    // for (int i = 0; i < 16; i++) {
+    //     result[i] = a[i] ^ b[i] ^ c[i] ^ d[i];
+    // }
+    // result[16] = '\0';
+    // return;
 }
 
 void printRaid5Disks(void){
@@ -48,6 +106,7 @@ void restoreData (void){
 }
 
 int main(int argc, char *argv[]){// need only 3 stripes, so 16 blocks(12 data, 4 parity)
+    // input validation
     if (argc != 2) {
         printf("Invalid number of arguments, run ./Raid5 with a disk number (0-4).\n");
         return 1;
@@ -59,19 +118,11 @@ int main(int argc, char *argv[]){// need only 3 stripes, so 16 blocks(12 data, 4
         printf("Invalid disk number.Disk number should be between 0-4.\n");
         return 1;
     }
-
-    char input[17];
-    int i = 1;
-    
-
-    while (i<=48 && scanf("%16c", input) == 1){
-        input[16] = '\0';
-        
-        printf("%d.[%s]\n", i, input);
-        i++;
-    }
-
-    printRaid5Disks();
+    // start to read input and save to array
+    char *data = malloc(769 * sizeof(char));
+    scanf("%768c", data);
+    createRaid5(data, 3, 5);
+   // printRaid5Disks();
 
 //fail code
     simulateFailure(failed_disk);
